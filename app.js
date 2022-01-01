e39b5cf37fe044f6a73ed100d8d28d2e254f5825
CarlbertCannon <providenceneedschange@gmail.com>
2022-01-01 10:26:25 -0700

    intialzed changes

--- app.js
+++ app.js
@@ -4,7 +4,22 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Carlbert";
+let lastName = "Cannon";
+var age = "37";
 
 // Exercise 2
+let fullName = firstName + " " + lastName;
+console.log("fullName: ", fullName);
+
+//Bonus
+let templatefullName = `${firstName} ${lastName}`;
+console.log("templatefullName: ", templatefullName);
 
 // Exercise 3
+let city = "Durango";
+let pastTime = "working out, by way of hiking, body weight exercises, running";
+let myStory = `Hi my name is ${fullName}. I live in ${city}. I enjoy ${pastTime} and coding!`;
+console.log(myStory);
+
+
